52a45d75a882dc72fdeb36b856921848c7661777
Blacklion14 <73174529+Blacklion14@users.noreply.github.com>
2020-11-25 13:25:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const DownScroll =()=>{
+    window.addEventListener("scroll",function(){
+      var header = document.querySelector(".navbar");
+      header.classList.toggle("shadow", window.scrollY > 0);
+    } );
+}
+
+DownScroll();
+
