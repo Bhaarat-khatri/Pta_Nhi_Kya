8f2d89259ae6476899fb5393bad8416a96d320aa
Blacklion14 <73174529+Blacklion14@users.noreply.github.com>
2020-12-01 16:52:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
+// var loader = document.querySelector('.preloader');
+
+// window.addEventListener('load', function () {
+//   loader.style.display = 'none';
+//     // loader.parentElement.removeChild(loader);
+// });
+
 
 const DownScroll =()=>{
     window.addEventListener("scroll",function(){
       var header = document.querySelector(".navbar");
       header.classList.toggle("shadow", window.scrollY > 0);
+      header.classList.toggle("fixed", window.scrollY > 0);
     } );
 }
 
 DownScroll();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
